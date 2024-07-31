5d5a69452b7ec8be8d3a90ef7f73afc4ee2bf70a
tugberk.varoglu <tugberkvaroglu03@gmail.com>
2024-07-31 23:37:53 +0300

    imported css into the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import "./App.css";
 import Login from './components/Login';
 import Main from './components/main';
 import Profile from './components/profile';
