5a08abe72091a042536e1b840a1821195453d23d
tugberk.varoglu <tugberkvaroglu03@gmail.com>
2024-08-07 22:55:53 +0300

    name change of the Main.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import "./App.css";
 import Login from './components/Login';
-import Main from './components/main';
+import Main from './components/Main';
 import Profile from './components/profile';
 import Register from './components/Register';
 
